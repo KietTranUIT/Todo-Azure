e2a17218260bec78887f9e4693a4221144259443
Tran Kiet <kiettranuit@gmail.com>
2023-12-02 07:57:59 +0700

    update clone app

--- main.go
+++ main.go
@@ -1,34 +1,16 @@
 package main
 
 import (
-	"errors"
-	"github.com/ichtrojan/go-todo/routes"
-	"github.com/ichtrojan/thoth"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/ichtrojan/go-todo/routes"
 )
 
 func main() {
-	logger, _ := thoth.Init("log")
-
-	if err := godotenv.Load(); err != nil {
-		logger.Log(errors.New("no .env file found"))
-		log.Fatal("No .env file found")
-	}
-
-	port, exist := os.LookupEnv("PORT")
-
-	if !exist {
-		logger.Log(errors.New("PORT not set in .env"))
-		log.Fatal("PORT not set in .env")
-	}
-
-	err := http.ListenAndServe(":"+port, routes.Init())
+	err := http.ListenAndServe(":8080", routes.Init())
 
 	if err != nil {
-		logger.Log(err)
-		log.Fatal(err)
+		log.Println("Loi: ", err.Error())
 	}
 }
